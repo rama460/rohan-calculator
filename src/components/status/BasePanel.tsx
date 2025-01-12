80fee7afd18641b5543e1f16d253dcea0b389e02
rama460 <lokiaqsw345@gmail.com>
2025-01-12 16:39:18 +0900

    Add title field

--- src/components/status/BasePanel.tsx
+++ src/components/status/BasePanel.tsx
@@ -1,7 +1,7 @@
 import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
 import React from "react";
 import { races } from "../static/racs";
-
+import Grid from "@mui/material/Grid2";
 interface BasePanelProps {
     level: number;
     heroLevel: number;
@@ -24,77 +24,83 @@ export const BasePanel: React.FC<BasePanelProps> = ({ level, heroLevel, raceid, 
 
     return (
         <React.Fragment>
-            <Box display="flex" alignItems="center" gap={2}>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <Typography variant="body1" sx={{ textAlign: "left" }}>
-                        Lv:
-                    </Typography>
-                    <TextField
-                        type="number"
-                        size="small"
-                        defaultValue={115}
-                        sx={{ width: "80px", }}
-                        slotProps={{ htmlInput: { min: 1, max: 115 } }}
-                        onChange={(event) => {
-                            setLevel(Number(event.target.value));
-                        }}
-                    />
-                </Box>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <Typography variant="body1" sx={{ textAlign: "left" }}>
-                        征服者レベル:
-                    </Typography>
-                    <TextField
-                        type="number"
-                        size="small"
-                        defaultValue={50}
-                        value={heroLevel}
-                        sx={{ width: "80px", }}
-                        slotProps={{ htmlInput: { min: 0, max: 50 } }}
-                        onChange={(event) => {
-                            setHeroLevel(Number(event.target.value));
-                        }}
-                    />
-                </Box>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <Typography variant="body1" sx={{ textAlign: "left" }}>
-                        種族:
-                    </Typography>
-                    <FormControl fullWidth size="small" sx={{ margin: "10px" }}>
-                        <InputLabel id="demo-simple-select-label">Name</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={raceid}
-                            label="Name"
-                            onChange={handleRaceChange}
-                        >
-                            {races.map((race) => (
-                                <MenuItem value={race.id}>{race.name}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>                </Box>
-                <Box display="flex" alignItems="center" gap={2}>
-                    <Typography variant="body1" sx={{ textAlign: "left" }}>
-                        職業:
-                    </Typography>
-                    <FormControl fullWidth size="small" sx={{ margin: "10px" }}>
-                        <InputLabel id="demo-simple-select-label">Name</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={jobid}
-                            label="Name"
-                            disabled={level < 50}
-                            onChange={handleJobChange}
-                        >
-                            {races[Number(raceid)].jobs.map((job) => (
-                                <MenuItem value={job.id}>{job.name}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Box>
+            <Grid container columnSpacing={2}>
+                <Grid size={{ md: 6, xs: 12 }}>
+                    <Box display="flex" alignItems="center" justifyContent={"space-between"}>
+                        <Typography variant="body1" sx={{ textAlign: "left" }}>
+                            Lv:
+                        </Typography>
+                        <TextField
+                            type="number"
+                            size="small"
+                            defaultValue={115}
+                            sx={{ width: "80px", }}
+                            slotProps={{ htmlInput: { min: 1, max: 115 } }}
+                            onChange={(event) => {
+                                setLevel(Number(event.target.value));
+                            }}
+                        />
+                        <Typography variant="body1" sx={{ textAlign: "left" }}>
+                            征服者レベル:
+                        </Typography>
+                        <TextField
+                            type="number"
+                            size="small"
+                            defaultValue={50}
+                            value={heroLevel}
+                            sx={{ width: "80px", }}
+                            slotProps={{ htmlInput: { min: 0, max: 50 } }}
+                            onChange={(event) => {
+                                setHeroLevel(Number(event.target.value));
+                            }}
+                        />
+                    </Box>
+                </Grid>
+                <Grid size={{ md: 6, xs: 12 }}>
+                    <Box display="flex" alignItems="center" justifyContent={"space-between"}>
+                        <Typography variant="body1" sx={{ textAlign: "left" }}>
+                            種族:
+                        </Typography>
+                        <FormControl size="small">
+                            <Select
+                                value={raceid}
+                                onChange={handleRaceChange}
+                            >
+                                {races.map((race) => (
+                                    <MenuItem value={race.id}>{race.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <Typography variant="body1" sx={{ textAlign: "left" }}>
+                            職業:
+                        </Typography>
+                        <FormControl size="small">
+                            <Select
+                                value={jobid}
+                                disabled={level < 50}
+                                onChange={handleJobChange}
+                            >
+                                {races[Number(raceid)].jobs.map((job) => (
+                                    <MenuItem value={job.id}>{job.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+                <Grid size={{ md: 6, xs: 12 }}>
+                    <Box display="flex" alignItems="center" gap={2}>
+                        <Typography variant="body1" sx={{ textAlign: "left", width: "48px" }}>
+                            称号:
+                        </Typography>
+                        <FormControl size="small" >
+                            <Select
+                                value={jobid}
+                            >
+                            </Select>
+                        </FormControl>
+                    </Box>
+                </Grid>
+            </Grid>
         </React.Fragment>
     );
 }
