f4593fcb4a9c5c20cf3b49a834d28a73c7aa422f
rama460 <lokiaqsw345@gmail.com>
2025-01-20 02:02:55 +0900

    Fix tooltip position bug if scrolled

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -20,13 +20,14 @@ export const Tooltip: React.FC<Props> = memo((props) => {
     };
     const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
         if (!ref.current) return;
-        ref.current.style.top = `${e.clientY - 100}px`;
-        ref.current.style.left = `${e.clientX}px`;
+        ref.current.style.top = `${e.clientY - 50}px`;
+        ref.current.style.left = `${e.clientX + 50}px`;
     }
     const contentStyle: React.CSSProperties = {
-        position: "absolute",
+        position: "fixed",
         opacity: 0,
         visibility: "hidden",
+        zIndex: 1000,
     }
     return (
         <div>
