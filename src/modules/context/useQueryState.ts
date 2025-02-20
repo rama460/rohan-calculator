bd1044a131748af4e28a6752732211329e6737df
rama460 <lokiaqsw345@gmail.com>
2025-02-20 23:44:04 +0900

    Compression implementaion prototype

--- src/modules/context/useQueryState.ts
+++ src/modules/context/useQueryState.ts
@@ -1,22 +1,31 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 
+
+
+export default useQueryObject;
+import LZString from "lz-string";
+
 function useQueryObject<T>(key: string, defaultValue: T) {
     const [searchParams, setSearchParams] = useSearchParams();
     const savedValue = searchParams.get(key);
 
-    const initialValue = savedValue ? JSON.parse(decodeURIComponent(savedValue)) : defaultValue;
+    // 圧縮データを解凍
+    const initialValue = savedValue
+        ? JSON.parse(LZString.decompressFromEncodedURIComponent(savedValue)) : defaultValue;
+
     const [state, setState] = useState<T>(initialValue);
 
     useEffect(() => {
+        const compressed = LZString.compressToEncodedURIComponent(
+            JSON.stringify(state)
+        );
         setSearchParams((prev) => {
             const newParams = new URLSearchParams(prev);
-            newParams.set(key, encodeURIComponent(JSON.stringify(state)));
+            newParams.set(key, compressed);
             return newParams;
         });
     }, [key, state, setSearchParams]);
 
     return [state, setState] as const;
 }
-
-export default useQueryObject;
