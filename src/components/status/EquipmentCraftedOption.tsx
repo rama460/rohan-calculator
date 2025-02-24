b5cadecf86a687738cea6d840d0607f245d7bebd
rama460 <lokiaqsw345@gmail.com>
2025-02-24 19:35:19 +0900

    Add some new component

---
+++ src/components/status/EquipmentCraftedOption.tsx
@@ -0,0 +1,67 @@
+import { Box, Button, FormControl, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
+import { BuiltinOptionKeyType, BuiltinOptions, getCraftedOptions } from "../static/options";
+import RemoveIcon from '@mui/icons-material/Remove';
+import React from "react";
+import { Equipments } from "../../modules/context/useEquipmentsContext";
+
+interface EquipmentCraftedOptionProps {
+    name: BuiltinOptionKeyType;
+    value: number;
+    equipmentType: keyof Equipments;
+    index: number;
+    options: { name: BuiltinOptionKeyType, value: number }[];
+    setOptions: (options: { name: BuiltinOptionKeyType, value: number }[]) => void;
+}
+export const EquipmentCraftedOption: React.FC<EquipmentCraftedOptionProps> = ({ name, value, equipmentType, index, options, setOptions }) => {
+    const EquipmentCraftedOptionStyle: React.CSSProperties = {
+        margin: "10px",
+    }
+    const handleOptionChange = (event: SelectChangeEvent) => {
+        // assign the new value to the same index as the old value
+        options[index] = {
+            name: event.target.value as BuiltinOptionKeyType,
+            value: getCraftedOptions(equipmentType)[event.target.value as BuiltinOptionKeyType] ?? 0
+        };
+        setOptions([...options])
+    }
+
+    const handleOptionRemove = () => {
+        // remove the option at the index
+        options.splice(index, 1);
+        setOptions([...options]);
+    }
+
+    return (
+        <Box display="flex" justifyContent="space-between" alignItems="center" gap={2} sx={EquipmentCraftedOptionStyle}>
+            <FormControl size="small">
+                <Select
+                    defaultValue={name}
+                    value={name}
+                    onChange={handleOptionChange}
+                >
+                    <MenuItem value="none">なし</MenuItem>
+                    {Object.keys(getCraftedOptions(equipmentType)).filter((n) => !(options.some((option) => option.name === n)) || n == name).map((name) => (
+                        <MenuItem value={name}>{BuiltinOptions[name as BuiltinOptionKeyType]?.displayName ?? "なし"}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
+                <TextField
+                    type="number"
+                    size="small"
+                    defaultValue={value}
+                    value={value}
+                    onChange={(event) => {
+                        options[index] = { name: name as BuiltinOptionKeyType, value: Number(event.target.value) };
+                        setOptions([...options]);
+                    }}
+                    sx={{ width: "100px", }}
+                />
+                <Button onClick={handleOptionRemove} >
+                    <RemoveIcon />
+                </Button>
+            </Box>
+        </Box >
+    );
+}
+export default EquipmentCraftedOption;
